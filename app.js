1454d36787087af56b90e6bd0505d0b7cbb48661
Colin Aten <caten@udel.edu>
2025-10-09 16:47:10 -0400

    get rid of post request

--- app.js
+++ app.js
@@ -113,45 +113,45 @@ app.get("/generate-initial", async (req, res) => {
   }
 });
 
-app.post("/generate-initial", async (req, res) => {
-  const { prompt } = req.body;
-  try {
-    const response = await gemini.models.generateContent({
-      model: "gemini-2.5-flash",
-      contents:
-        "Create 3 Unique Historical events that have a title, date, and description",
-      config: {
-        thinkingConfig: { thinkingBudget: 0 },
-        systemInstruction:
-          "You are a game revolving around letting users create alternate histories",
-        responseMimeType: "application/json",
-        responseSchema: {
-          type: Type.ARRAY,
-          maxItems: 3,
-          minItems: 3,
-          uniqueItems: true,
-          items: {
-            type: Type.OBJECT,
-            properties: {
-              title: {
-                type: Type.STRING,
-              },
-              date: {
-                type: Type.STRING,
-              },
-              description: {
-                type: Type.STRING,
-              },
-              propertyOrdering: ["title", "date", "description"],
-            },
-          },
-        },
-      },
-    });
-    res.json({ data: response });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
+// app.post("/generate-initial", async (req, res) => {
+//   const { prompt } = req.body;
+//   try {
+//     const response = await gemini.models.generateContent({
+//       model: "gemini-2.5-flash",
+//       contents:
+//         "Create 3 Unique Historical events that have a title, date, and description",
+//       config: {
+//         thinkingConfig: { thinkingBudget: 0 },
+//         systemInstruction:
+//           "You are a game revolving around letting users create alternate histories",
+//         responseMimeType: "application/json",
+//         responseSchema: {
+//           type: Type.ARRAY,
+//           maxItems: 3,
+//           minItems: 3,
+//           uniqueItems: true,
+//           items: {
+//             type: Type.OBJECT,
+//             properties: {
+//               title: {
+//                 type: Type.STRING,
+//               },
+//               date: {
+//                 type: Type.STRING,
+//               },
+//               description: {
+//                 type: Type.STRING,
+//               },
+//               propertyOrdering: ["title", "date", "description"],
+//             },
+//           },
+//         },
+//       },
+//     });
+//     res.json({ data: response });
+//   } catch (error) {
+//     res.status(500).json({ error: error.message });
+//   }
+// });
 
 app.listen(3000, () => console.log("Server running on port 3000"));
