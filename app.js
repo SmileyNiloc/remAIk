94ab60cde03475b8bbf447d73f63982d64d86e1f
Colin Aten <caten@udel.edu>
2025-10-20 20:47:57 -0400

    made changes

--- app.js
+++ app.js
@@ -77,9 +77,9 @@ app.post("/extend-timeline", verifyFirebaseToken, async (req, res) => {
         systemInstruction: sysInstr,
         thinkingConfig: { thinkingBudget: 0 },
         responseMimeType: "application/json",
-        temperature: 1.2,
-        topP: 0.9,
-        topK: 40,
+        temperature: 2.0,
+        topP: 1.0,
+        topK: 64,
         responseSchema: {
           type: Type.ARRAY,
           maxItems: 2,
